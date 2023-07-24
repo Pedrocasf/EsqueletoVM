8230abf40958d3230c33b60a7936d1049586be1d
Pedrocasf <pedrostarling2000@gmail.com>
2023-07-24 03:55:53 -0300

    begin interpreter

--- src/RV32I/RV32I.h
+++ src/RV32I/RV32I.h
@@ -12,13 +12,11 @@
 #include <errno.h>
 #include "async.h"
 typedef struct VM_state{
-    uint32_t pc;
+    uint32_t* pc;
     uint32_t* x;
-     void* memory;
+    void* memory;
     uint32_t* csr;
 }VM_state;
 void build_vm_state(VM_state** state, char* rom_name);
-typedef uint32_t (*VM_Call)(uint32_t pc, void* memory);
-typedef VM_Call* (*VM_Instruction)(uint32_t pc, uint32_t *x, void* memory);
 void begin(VM_state* state);
 #endif //ESQUELETOVM_RV32I_H
